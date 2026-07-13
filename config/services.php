075388cf9cc02863e1f5df928dfe896ae10e72c5
vinhthuongg <vinhthuong995@gmail.com>
2026-07-13 17:27:33 +0700

    Remove public CRM access links

--- config/services.php
+++ config/services.php
@@ -43,14 +43,6 @@ return [
         'knowledge_webhook_url' => env('BOTPRESS_KNOWLEDGE_WEBHOOK_URL'),
         'knowledge_webhook_secret' => env('BOTPRESS_KNOWLEDGE_WEBHOOK_SECRET'),
     ],
-    'chat_webview' => [
-        'token' => env('CHAT_WEBVIEW_TOKEN', env('BOTPRESS_ADMIN_TOKEN')),
-    ],
-    'public_admin' => [
-        'enabled' => env('PUBLIC_ADMIN_ACCESS_ENABLED', false),
-        'token' => env('PUBLIC_ADMIN_ACCESS_TOKEN'),
-        'email' => env('PUBLIC_ADMIN_ACCESS_EMAIL', 'admin@oldthread.store'),
-    ],
     'nim' => [
         'api_key' => env('NVIDIA_NIM_API_KEY', env('NVIDIA_API_KEY')),
         'base_url' => env('NVIDIA_NIM_BASE_URL', 'https://integrate.api.nvidia.com/v1'),
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\AuthController;
-use App\Http\Controllers\Web\ChatWebviewController;
 use App\Http\Controllers\Web\DashboardController;
 use App\Http\Controllers\Web\MessengerController;
-use App\Http\Controllers\Web\PublicAdminAccessController;
 use App\Http\Controllers\Web\WorkShiftController;
 use Modules\Customer\Http\Controllers\CustomerPageController;
 
@@ -20,9 +18,6 @@ Route::get('/', function () {
 Route::view('privacy-policy', 'legal.privacy')->name('legal.privacy');
 Route::view('terms-of-service', 'legal.terms')->name('legal.terms');
 Route::view('data-deletion', 'legal.data-deletion')->name('legal.data-deletion');
-Route::get('chat-webview/{token}', [ChatWebviewController::class, 'index'])->name('chat-webview.index');
-Route::get('chat-webview/{token}/conversations/{conversation}', [ChatWebviewController::class, 'show'])->name('chat-webview.show');
-Route::get('public-admin/{token}', PublicAdminAccessController::class)->name('public-admin.launch');
 
 Route::middleware('guest')->group(function (): void {
     Route::get('login', [AuthController::class, 'create'])->name('login');
